923145f9bbeca064d3e91bc4e662b1989226ff5f
zaidanalbert <m006d4ky2194@bangkit.academy>
2024-03-05 15:14:30 +0700

    Create streamlit-app.py

---
+++ streamlit-app.py
@@ -0,0 +1,184 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+from babel.numbers import format_currency
+sns.set(style='dark')
+
+def create_daily_orders_df(df):
+        daily_orders_df = df.resample(rule='D', on='order_date').agg({
+            "order_id": "nunique",
+            "total_price": "sum"
+        })
+        daily_orders_df = daily_orders_df.reset_index()
+        daily_orders_df.rename(columns={
+            "order_id": "order_count",
+            "total_price": "revenue"
+        }, inplace=True)
+        
+        return daily_orders_df
+    
+def create_sum_order_items_df(self):
+    sum_order_items_df = self.df.groupby("product_category_name_english")["product_id"].count().reset_index()
+    sum_order_items_df.rename(columns={
+        "product_id": "product_count"
+    }, inplace=True)
+    sum_order_items_df = sum_order_items_df.sort_values(by='product_count', ascending=False)
+    
+    return sum_order_items_df
+    
+def review_score_df(self):
+    review_scores = self.df['review_score'].value_counts().sort_values(ascending=False)
+    most_common_score = review_scores.idxmax()
+
+    return review_scores, most_common_score
+
+def create_bystate_df(self):
+        bystate_df = self.df.groupby(by="customer_state").customer_id.nunique().reset_index()
+        bystate_df.rename(columns={
+            "customer_id": "customer_count"
+        }, inplace=True)
+        most_common_state = bystate_df.loc[bystate_df['customer_count'].idxmax(), 'customer_state']
+        bystate_df = bystate_df.sort_values(by='customer_count', ascending=False)
+
+        return bystate_df, most_common_state
+
+all_df = pd.read_csv("all_data.csv")
+
+datetime_columns = ["order_approved_at", "order_delivered_carrier_date", "order_delivered_customer_date", "order_estimated_delivery_date", "order_purchase_timestamp", "shipping_limit_date"]
+all_df.sort_values(by="order_date", inplace=True)
+all_df.reset_index(inplace=True)
+ 
+for column in datetime_columns:
+    all_df[column] = pd.to_datetime(all_df[column])
+
+min_date = all_df["order_date"].min()
+max_date = all_df["order_date"].max()
+ 
+with st.sidebar:
+    # Title
+    st.title("Muhammad Zaidan Albert")
+    # Menambahkan logo perusahaan
+    st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png")
+    
+    # Mengambil start_date & end_date dari date_input
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',min_value=min_date,
+        max_value=max_date,
+        value=[min_date, max_date]
+    )
+
+daily_orders_df = create_daily_orders_df()
+sum_order_items_df = create_sum_order_items_df()
+review_score, common_score = review_score_df()
+state, most_common_state = create_bystate_df()
+
+st.header('Dicoding E-Commerce Dashboard :convenience_store:')
+
+# Daily Orders
+st.subheader("Daily Orders")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    total_order = daily_orders_df["order_count"].sum()
+    st.markdown(f"Total Order: **{total_order}**")
+
+with col2:
+    total_revenue = format_currency(daily_orders_df["revenue"].sum(), "IDR", locale="id_ID")
+    st.markdown(f"Total Revenue: **{total_revenue}**")
+
+fig, ax = plt.subplots(figsize=(12, 6))
+ax.plot(
+    daily_orders_df["order_approved_at"],
+    daily_orders_df["order_count"],
+    marker="o",
+    linewidth=2,
+    color="#90CAF9"
+)
+ax.tick_params(axis="x", rotation=45)
+ax.tick_params(axis="y", labelsize=15)
+st.pyplot(fig)
+
+# Order Items
+st.subheader("Order Items")
+col1, col2 = st.columns(2)
+
+with col1:
+    total_items = sum_order_items_df["product_count"].sum()
+    st.markdown(f"Total Items: **{total_items}**")
+
+with col2:
+    avg_items = sum_order_items_df["product_count"].mean()
+    st.markdown(f"Average Items: **{avg_items}**")
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(45, 25))
+
+colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+
+sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.head(5), palette=colors, ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("Number of Sales", fontsize=30)
+ax[0].set_title("Produk paling banyak terjual", loc="center", fontsize=50)
+ax[0].tick_params(axis ='y', labelsize=35)
+ax[0].tick_params(axis ='x', labelsize=30)
+
+sns.barplot(x="product_count", y="product_category_name_english", data=sum_order_items_df.sort_values(by="product_count", ascending=True).head(5), palette=colors, ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("Number of Sales", fontsize=30)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].set_title("Produk paling sedikit terjual", loc="center", fontsize=50)
+ax[1].tick_params(axis='y', labelsize=35)
+ax[1].tick_params(axis='x', labelsize=30)
+
+st.pyplot(fig)
+
+# Review Score
+st.subheader("Review Score")
+col1,col2 = st.columns(2)
+
+with col1:
+    avg_review_score = review_score.mean()
+    st.markdown(f"Average Review Score: **{avg_review_score}**")
+
+with col2:
+    most_common_review_score = review_score.value_counts().index[0]
+    st.markdown(f"Most Common Review Score: **{most_common_review_score}**")
+
+fig, ax = plt.subplots(figsize=(12, 6))
+sns.barplot(x=review_score.index, 
+            y=review_score.values, 
+            order=review_score.index,
+            palette=["#90CAF9" if score == common_score else "#D3D3D3" for score in review_score.index]
+            )
+
+plt.title("Rating by customers for service", fontsize=15)
+plt.xlabel("Rating")
+plt.ylabel("Count")
+plt.xticks(fontsize=12)
+st.pyplot(fig)
+
+# Customer Demographic
+st.subheader("Customer Demographic")
+tab1 = st.tabs(["State"])
+
+with tab1:
+    most_common_state = state.customer_state.value_counts().index[0]
+    st.markdown(f"Most Common State: **{most_common_state}**")
+
+    fig, ax = plt.subplots(figsize=(12, 6))
+    sns.barplot(x=state.customer_state.value_counts().index,
+                y=state.customer_count.values, 
+                data=state,
+                palette=["#90CAF9" if score == most_common_state else "#D3D3D3" for score in state.customer_state.value_counts().index]
+                    )
+
+    plt.title("Number customers from State", fontsize=15)
+    plt.xlabel("State")
+    plt.ylabel("Number of Customers")
+    plt.xticks(fontsize=12)
+    st.pyplot(fig)
+
+st.caption('Copyright (C) M. Zaidan A. 2024')
